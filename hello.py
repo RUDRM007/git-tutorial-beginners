fe716ea0682d2c0cd4911c5852579b8b4c76a9c8
Rudraansh Sharma <rudrajitesh.sharma@gmail.com>
2026-06-17 20:57:58 +0530

    updated hello

--- hello.py
+++ hello.py
@@ -6,3 +6,4 @@ print("Hello, World!")
 print("This is a Python file for hello.")
 print("We are learning how to use Git for version control.")
 print("Git is a powerful tool for managing code and collaborating with others.")
+# changing the code to see if it works
