a2f07a5c2264ed93501c9f43cf64fcc2c1ab1331
Rudraansh Sharma <rudrajitesh.sharma@gmail.com>
2026-06-17 20:54:10 +0530

    Design complete

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # This is python file for hello
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 print("Hello, World!")
 print("This is a Python file for hello.")
+print("We are learning how to use Git for version control.")
+print("Git is a powerful tool for managing code and collaborating with others.")
